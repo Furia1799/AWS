74ad23f00397498f6f64d7724b8539e3659a1e6a
BRYAN REYES <bryan.reyes@alumnos.udg.mx>
2020-04-22 18:19:06 -0500

    Agregar carpeta rmi_nombre

---
+++ RMI_Nombre/Cliente.java
@@ -0,0 +1,35 @@
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Scanner;
+
+public class Cliente {
+
+    private static final String IP = "localhost"; // Puedes cambiar a localhost
+    private static final int PUERTO = 1100; //Si cambias aquí el puerto, recuerda cambiarlo en el servidor
+
+    public static void main(String[] args) throws RemoteException, NotBoundException {
+        Registry registry = LocateRegistry.getRegistry(IP, PUERTO);
+        Interfaz interfaz = (Interfaz) registry.lookup("Ejemplo"); //Buscar en el registro...
+
+        Scanner sc = new Scanner(System.in);
+        int eleccion = 1;
+        String respuesta;
+        String nombre = "";
+
+        do {
+
+            System.out.println("Ingrese su nombre a continuacion :");
+            nombre = sc.nextLine();
+
+            respuesta = interfaz.hola(nombre);
+
+            System.out.println("Respuesta => " + respuesta);
+            System.out.println("Presiona ENTER para continuar");
+            sc.nextLine();
+
+        } while (eleccion != -1);
+    }
+}
