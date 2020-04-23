c9325e594a775dcc4a95fa8b6019e80ac7e64b17
BRYAN REYES <bryan.reyes@alumnos.udg.mx>
2020-04-22 23:44:08 -0500

    suma de rmi

--- RMI_Calculadora/Cliente.java
+++ RMI_Calculadora/Cliente.java
@@ -4,8 +4,8 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
 public class Cliente {
-	private static final String IP = "172.31.47.38"; // Puedes cambiar a localhost
-	private static final int PUERTO = 1101; //Si cambias aquí el puerto, recuerda cambiarlo en el servidor
+	private static final String IP = "localhost"; // Puedes cambiar a localhost
+	private static final int PUERTO = 1100; //Si cambias aquí el puerto, recuerda cambiarlo en el servidor
 	
     public static void main(String[] args) throws RemoteException, NotBoundException {
         Registry registry = LocateRegistry.getRegistry(IP, PUERTO);
